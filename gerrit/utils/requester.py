99f257fe61543d395b4a06c7b63e4b2f23b8ca33
shijl0925 <18221510672@139.com>
2024-03-12 11:36:36 +0800

    remove six package

--- gerrit/utils/requester.py
+++ gerrit/utils/requester.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # @Author: Jialiang Shi
-import six.moves.urllib.parse as urlparse
+import urllib.parse as urlparse
 from requests import Session
 from requests.adapters import HTTPAdapter
 from gerrit.utils.exceptions import (
